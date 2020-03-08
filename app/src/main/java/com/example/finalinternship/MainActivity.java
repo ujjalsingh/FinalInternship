528b785d2d3d80ae106d95497cef56abfc63325b
ujjalsingh <singhujjal100@gmail.com>
2020-03-08 13:17:30 -0400

    login  design

--- app/src/main/java/com/example/finalinternship/MainActivity.java
+++ app/src/main/java/com/example/finalinternship/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.finalinternship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import javax.security.auth.login.LoginException;
 
 public class MainActivity extends AppCompatActivity {
+    Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        click=findViewById(R.id.clickbtn);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
